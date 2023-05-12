598cc525d735dc74eb20d7657dbedae0ea51af25
Marley <marley.nodejs@gmail.com>
2023-05-12 19:07:16 +0300

    lint: fix lint errors and warnings

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,7 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import AppModule from './app/app.module';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  // eslint-disable-next-line no-console
+  .catch((err) => console.error(err));
