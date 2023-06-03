3c3a38ec130946549aefcf0e4b17ff4b2219d6e9
Marley <marley.nodejs@gmail.com>
2023-06-03 19:56:06 +0300

    feat: add add service and implementation

--- app/src/app/modules/courses/components/courses-list/courses-list.component.ts
+++ app/src/app/modules/courses/components/courses-list/courses-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import CoursesService from 'src/app/modules/courses/services/courses.service';
 import { CourseInterface } from 'src/app/modules/courses/types/course.interface';
 
 @Component({
@@ -10,54 +11,10 @@ export default class CoursesListComponent implements OnInit {
   public courses: CourseInterface[] = [];
   public searchText: string = '';
 
+  constructor(private readonly coursesService: CoursesService) {}
+
   ngOnInit(): void {
-    this.courses = [
-      {
-        id: 11,
-        name: 'БС Диасофт Angular',
-        creationDate: new Date(2023, 4, 29),
-        durationMinutes: 64 * 60,
-        description:
-          'Курс подготовлен в компании Диасофт для начинающих разработчиков Angular',
-        topRated: true,
-      },
-      {
-        id: 12,
-        name: 'БС Диасофт Аналитик',
-        creationDate: new Date(2022, 11, 11),
-        durationMinutes: 64 * 60 + 30,
-        description:
-          'Курс подготовлен в компании Диасофт для начинающих Аналитиков',
-        topRated: false,
-      },
-      {
-        id: 13,
-        name: 'БС Диасофт Qpalette',
-        creationDate: new Date(2022, 12, 12),
-        durationMinutes: 55,
-        description:
-          'Курс подготовлен в компании Диасофт для начинающих разработчиков Qpalette',
-        topRated: true,
-      },
-      {
-        id: 14,
-        name: 'БС Диасофт Java разработчик',
-        creationDate: new Date(2022, 9, 11),
-        durationMinutes: 50 * 60 + 30,
-        description:
-          'Курс подготовлен в компании Диасофт для начинающих Java разработчиков',
-        topRated: false,
-      },
-      {
-        id: 14,
-        name: 'БС Диасофт QBPM',
-        creationDate: new Date(2023, 5, 1),
-        durationMinutes: 20 * 60,
-        description:
-          'Курс подготовлен в компании Диасофт для начинающих разбираться в QBPM',
-        topRated: false,
-      },
-    ];
+    this.courses = this.coursesService.getCourses();
   }
 
   public onSearchTextEntered(searchValue: string) {
--- app/src/app/modules/courses/services/courses.service.ts
+++ app/src/app/modules/courses/services/courses.service.ts
@@ -1,9 +1,71 @@
 import { Injectable } from '@angular/core';
+import { CourseInterface } from 'src/app/modules/courses/types/course.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class CoursesService {
+export default class CoursesService {
+  private courses: CourseInterface[] = [
+    {
+      id: 11,
+      name: 'БС Диасофт Angular',
+      creationDate: new Date(2023, 4, 29),
+      durationMinutes: 64 * 60,
+      description:
+        'Курс подготовлен в компании Диасофт для начинающих разработчиков Angular',
+      topRated: true,
+    },
+    {
+      id: 12,
+      name: 'БС Диасофт Аналитик',
+      creationDate: new Date(2022, 11, 11),
+      durationMinutes: 64 * 60 + 30,
+      description:
+        'Курс подготовлен в компании Диасофт для начинающих Аналитиков',
+      topRated: false,
+    },
+    {
+      id: 13,
+      name: 'БС Диасофт Qpalette',
+      creationDate: new Date(2022, 12, 12),
+      durationMinutes: 55,
+      description:
+        'Курс подготовлен в компании Диасофт для начинающих разработчиков Qpalette',
+      topRated: true,
+    },
+    {
+      id: 14,
+      name: 'БС Диасофт Java разработчик',
+      creationDate: new Date(2022, 9, 11),
+      durationMinutes: 50 * 60 + 30,
+      description:
+        'Курс подготовлен в компании Диасофт для начинающих Java разработчиков',
+      topRated: false,
+    },
+    {
+      id: 15,
+      name: 'БС Диасофт QBPM',
+      creationDate: new Date(2023, 5, 1),
+      durationMinutes: 20 * 60,
+      description:
+        'Курс подготовлен в компании Диасофт для начинающих разбираться в QBPM',
+      topRated: false,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
+
+  public getCourses(): CourseInterface[] {
+    return this.courses;
+  }
+
+  public getCourseById(id: number): CourseInterface {
+    return this.getCourses()[0];
+  }
+
+  public createCourse(course: CourseInterface): void {}
+
+  public updateCourse(course: CourseInterface): void {}
+
+  public removeCourse(course: CourseInterface): void {}
 }
