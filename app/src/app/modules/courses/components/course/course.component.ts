0cf1c0f996c556ee419e6bc737d941134053d252
Marley <marley.nodejs@gmail.com>
2023-06-18 16:15:16 +0300

    feat: save progress

--- app/src/app/modules/courses/components/course/course.component.ts
+++ app/src/app/modules/courses/components/course/course.component.ts
@@ -1,54 +1,26 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  EventEmitter,
-  Input,
-  Output,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CourseInterface } from 'src/app/modules/courses/types/course.interface';
-
-import { ConfirmationService, PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-dia-course',
   templateUrl: './course.component.html',
   styleUrls: [],
-  providers: [ConfirmationService],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class CourseComponent {
   @Input() public course: CourseInterface = {} as CourseInterface;
 
-  @Output() public edit: EventEmitter<CourseInterface> =
-    new EventEmitter<CourseInterface>();
+  // @Output() public edit: EventEmitter<CourseInterface> =
+  //   new EventEmitter<CourseInterface>();
 
-  @Output() public delete: EventEmitter<CourseInterface> =
-    new EventEmitter<CourseInterface>();
+  // @Output() public delete: EventEmitter<CourseInterface> =
+  //   new EventEmitter<CourseInterface>();
 
-  public editCourse(): void {
-    this.edit.emit(this.course);
-  }
+  // public editCourse(): void {
+  //   this.edit.emit(this.course);
+  // }
 
-  public onApproveCourseDeletion(): void {
-    this.delete.emit(this.course);
-  }
-
-  constructor(
-    private confirmationService: ConfirmationService,
-    private primengConfig: PrimeNGConfig
-  ) {}
-
-  showConfirmDeletionDialog() {
-    this.confirmationService.confirm({
-      header: `Подтверждаете удаление курса ` + this.course.name + ' ?',
-      message:
-        'Нажмите на кнопку "Да", для подтверждения удаления, либо "Нет" для отмены операции.',
-      icon: 'pi pi-exclamation-triangle',
-      rejectLabel: 'Нет',
-      rejectButtonStyleClass: 'p-button-text',
-      accept: () => this.onApproveCourseDeletion(),
-      acceptLabel: 'Да',
-      acceptButtonStyleClass: 'p-button-danger',
-    });
-  }
+  // public onApproveCourseDeletion(): void {
+  //   this.delete.emit(this.course);
+  // }
 }
--- app/src/app/modules/courses/components/courses-list/courses-list.component.ts
+++ app/src/app/modules/courses/components/courses-list/courses-list.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfirmationService, PrimeNGConfig } from 'primeng/api';
 import CoursesService from 'src/app/modules/courses/services/courses.service';
 import { CourseInterface } from 'src/app/modules/courses/types/course.interface';
 
 @Component({
   selector: 'app-dia-courses-list',
   templateUrl: './courses-list.component.html',
+  providers: [ConfirmationService],
   styleUrls: ['./courses-list.component.scss'],
 })
 export default class CoursesListComponent implements OnInit {
   public courses: CourseInterface[] = [];
   public searchText: string = '';
 
-  constructor(private readonly coursesService: CoursesService) {}
+  constructor(
+    private readonly coursesService: CoursesService,
+    private confirmationService: ConfirmationService,
+    private primengConfig: PrimeNGConfig
+  ) {}
 
   ngOnInit(): void {
     this.courses = this.coursesService.getCourses();
@@ -33,4 +39,26 @@ export default class CoursesListComponent implements OnInit {
     this.coursesService.removeCourse(course);
     this.courses = this.coursesService.getCourses();
   }
+
+  showConfirmDeletionDialog() {
+    // header: `Подтверждаете удаление курса ` + this.course.name + ' ?',
+    this.confirmationService.confirm({
+      header: `Подтверждаете удаление курса ?`,
+      message:
+        'Нажмите на кнопку "Да", для подтверждения удаления, либо "Нет" для отмены операции.',
+      icon: 'pi pi-exclamation-triangle',
+      rejectLabel: 'Нет',
+      rejectButtonStyleClass: 'p-button-text',
+      // accept: () => this.onApproveCourseDeletion(),
+      acceptLabel: 'Да',
+      acceptButtonStyleClass: 'p-button-danger',
+    });
+  }
+
+  // @Output() public delete: EventEmitter<CourseInterface> =
+  //   new EventEmitter<CourseInterface>();
+
+  // public onApproveCourseDeletion(): void {
+  //   this.delete.emit(this.course);
+  // }
 }
