643a4a7d8adb900b41089d655ddd42d73b3d98f1
Marley <marley.nodejs@gmail.com>
2023-06-18 17:38:40 +0300

    feat: add not-found page

--- app/src/app/routes/app-routing.module.ts
+++ app/src/app/routes/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import CoursesListComponent from 'src/app/modules/courses/components/courses-list/courses-list.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: CoursesListComponent,
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
