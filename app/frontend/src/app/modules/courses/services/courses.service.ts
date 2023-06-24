69260b91dd9eac8e55e419c4b54c54d55944f6c9
Marley <marley.nodejs@gmail.com>
2023-06-25 01:30:10 +0300

    feat: remove old list

--- app/frontend/src/app/modules/courses/services/courses.service.ts
+++ app/frontend/src/app/modules/courses/services/courses.service.ts
@@ -8,89 +8,6 @@ import { CourseInterface } from 'src/app/modules/courses/types/course.interface'
   providedIn: 'root',
 })
 export default class CoursesService {
-  private courses: CourseInterface[] = [
-    {
-      id: 11,
-      title: 'БС Диасофт Angular',
-      creationDate: new Date(2023, 4, 29),
-      duration: 64 * 60,
-      description:
-        'Курс подготовлен в компании Диасофт для начинающих разработчиков Angular',
-      topRated: true,
-      authors: [
-        {
-          id: 1370,
-          name: 'Polly',
-          lastName: 'Sosa',
-        },
-      ],
-    },
-    {
-      id: 12,
-      title: 'БС Диасофт Аналитик',
-      creationDate: new Date(2022, 11, 11),
-      duration: 64 * 60 + 30,
-      description:
-        'Курс подготовлен в компании Диасофт для начинающих Аналитиков',
-      topRated: false,
-      authors: [
-        {
-          id: 1370,
-          name: 'Polly',
-          lastName: 'Sosa',
-        },
-      ],
-    },
-    {
-      id: 13,
-      title: 'БС Диасофт Qpalette',
-      creationDate: new Date(2022, 12, 12),
-      duration: 55,
-      description:
-        'Курс подготовлен в компании Диасофт для начинающих разработчиков Qpalette',
-      topRated: true,
-      authors: [
-        {
-          id: 1370,
-          name: 'Polly',
-          lastName: 'Sosa',
-        },
-      ],
-    },
-    {
-      id: 14,
-      title: 'БС Диасофт Java разработчик',
-      creationDate: new Date(2022, 9, 11),
-      duration: 50 * 60 + 30,
-      description:
-        'Курс подготовлен в компании Диасофт для начинающих Java разработчиков',
-      topRated: false,
-      authors: [
-        {
-          id: 1370,
-          name: 'Polly',
-          lastName: 'Sosa',
-        },
-      ],
-    },
-    {
-      id: 15,
-      title: 'БС Диасофт QBPM',
-      creationDate: new Date(2023, 5, 1),
-      duration: 20 * 60,
-      description:
-        'Курс подготовлен в компании Диасофт для начинающих разбираться в QBPM',
-      topRated: false,
-      authors: [
-        {
-          id: 1370,
-          name: 'Polly',
-          lastName: 'Sosa',
-        },
-      ],
-    },
-  ];
-
   constructor(private readonly httpClient: HttpClient) {}
 
   public getCourses(): Observable<CourseInterface[]> {
@@ -104,7 +21,7 @@ export default class CoursesService {
   }
 
   public addCourse(newCourse: CourseInterface): void {
-    this.courses.push(newCourse);
+    // this.courses.push(newCourse);
   }
 
   public updateCourse(updatedCourse: CourseInterface): void {
