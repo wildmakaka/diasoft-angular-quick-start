2f439bf45ddddb9803dc1b9ebc4e76bc07614408
Marley <marley.nodejs@gmail.com>
2023-07-09 02:15:26 +0300

    fix: login error messages

--- app/frontend/src/app/modules/auth/components/login/login.component.ts
+++ app/frontend/src/app/modules/auth/components/login/login.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Subscription } from 'rxjs';
 import AuthService from 'src/app/modules/auth/services/auth.service';
 import { UserInterface } from 'src/app/modules/auth/types/user.interface';
 
@@ -9,19 +9,30 @@ import { UserInterface } from 'src/app/modules/auth/types/user.interface';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export default class LoginComponent {
+export default class LoginComponent implements OnInit, OnDestroy {
   @Input()
   isAuthenticated: boolean;
 
-  public loggedInUser$: Observable<UserInterface[]> =
-    this.authService.getLoggedInUser();
+  public loggedInUser: UserInterface[];
+
+  private loggedInUserSubscription: Subscription;
 
   constructor(
     private router: Router,
     private readonly authService: AuthService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loggedInUserSubscription = this.authService
+      .getLoggedInUser()
+      .subscribe((loggedInUser) => {
+        this.loggedInUser = loggedInUser;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.loggedInUserSubscription.unsubscribe();
+  }
 
   public login(username: string): void {
     // this.authService.login(username);
--- app/frontend/src/app/shared/components/breadcrumbs/breadcrumbs.component.ts
+++ app/frontend/src/app/shared/components/breadcrumbs/breadcrumbs.component.ts
@@ -43,7 +43,6 @@ export default class BreadcrumbsComponent implements OnInit, OnDestroy {
         });
       }
       this.gfg = links;
-      this.ngOnDestroy();
     }
   }
 
