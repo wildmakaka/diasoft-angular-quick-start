ae0b9eab0450821b4df7a4fb6bf8b8c50c4d7f49
Marley <marley.nodejs@gmail.com>
2023-07-09 22:01:26 +0300

    nothing

--- app/frontend/src/app/constants.ts
+++ app/frontend/src/app/constants.ts
@@ -1,3 +1,3 @@
+// export const API_SERVER: string = 'http://localhost:3001/api/v1';
 export const API_SERVER: string =
   'https://diasoft-angular-quick-start-backend.onrender.com/api/v1';
-// export const API_SERVER: string = 'http://localhost:3001/api/v1';
