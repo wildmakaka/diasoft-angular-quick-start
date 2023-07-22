24c442f1c0f57a8b90985c3f8ceddd5176c58eb6
Marley <marley.nodejs@gmail.com>
2023-07-22 07:14:19 +0300

    feat: add versel backend fix

--- app/frontend/src/app/constants.ts
+++ app/frontend/src/app/constants.ts
@@ -1,4 +1,4 @@
 export const API_SERVER: string =
   'https://diasoft-angular-quick-start-backend.vercel.app/api/v1';
-export const API_SERVER: string = 'http://localhost:3000/api/v1';
+// export const API_SERVER: string = 'http://localhost:3000/api/v1';
 // export const API_SERVER: string ='https://diasoft-angular-quick-start-backend.onrender.com/api/v1';
