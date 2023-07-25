dc5e1d6578450f189ee0fe2c7103327db37382c5
Marley <marley.nodejs@gmail.com>
2023-07-25 04:17:29 +0300

    save: progress

--- app/frontend/src/app/modules/auth/components/login-form/login-form.component.ts
+++ app/frontend/src/app/modules/auth/components/login-form/login-form.component.ts
@@ -9,20 +9,10 @@ import AuthService from 'src/app/modules/auth/services/auth.service';
 export default class LoginFormComponent {
   public login: string = '';
   public password: string = '';
-  // public isValidForm: boolean = false;
-
-  // submitted = false;
 
   constructor(private readonly authService: AuthService) {}
 
   onSubmit() {
-    // console.log('Submit');
-    // console.log(this.login);
-    // console.log(this.password);
-    // const userLogin = this.login;
-    // const userPassword = this.password;
-    // const userLogin = this.login.value.login;
-    // const userPassword = this.password.value.password;
     this.authService.login(this.login, this.password);
   }
 }
