a69b10bea732d056ff737a29f3a00980b4bf23cc
Marley <marley.nodejs@gmail.com>
2023-08-31 02:15:14 +0300

    save: progress

--- app/frontend/src/app/modules/courses/store/actions/getCourse.action.ts
+++ app/frontend/src/app/modules/courses/store/actions/getCourse.action.ts
@@ -2,11 +2,6 @@ import { createAction, props } from '@ngrx/store';
 import { ActionTypes } from 'src/app/modules/courses/store/actionTypes';
 import { CourseInterface } from 'src/app/modules/courses/types/course.interface';
 
-// export const getCourseAction = createAction(
-//   ActionTypes.GET_COURSE
-//   props<{ slug: string }>()
-// );
-
 export const getCourseAction = createAction(ActionTypes.GET_COURSE);
 
 export const getCourseSuccessAction = createAction(
